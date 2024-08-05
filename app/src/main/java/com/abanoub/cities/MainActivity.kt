3825255f4817199a501e6491c28218a08ec6917e
Abanoub Samir <abanoub.sameer.22@gmail.com>
2024-08-05 21:52:38 +0300

    Setup nav host

--- app/src/main/java/com/abanoub/cities/MainActivity.kt
+++ app/src/main/java/com/abanoub/cities/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.abanoub.cities.feature.cities.presentation.Cities
+import com.abanoub.cities.feature.cities.presentation.citiesScreen
 import com.abanoub.cities.ui.theme.CityFinderTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CityFinderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Cities) {
+                        citiesScreen(
+                            onCityClick = { city ->
+
+                            }
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CityFinderTheme {
-        Greeting("Android")
     }
 }
