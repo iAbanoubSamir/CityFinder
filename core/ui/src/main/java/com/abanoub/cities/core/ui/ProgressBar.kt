1ef5168ab68bcd4defaf9fcabb91b0da905e81ae
Abanoub Samir <abanoub.sameer.22@gmail.com>
2024-08-05 20:12:33 +0300

    Create circular progress

---
+++ core/ui/src/main/java/com/abanoub/cities/core/ui/ProgressBar.kt
@@ -0,0 +1,10 @@
+package com.abanoub.cities.core.ui
+
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CircularProgress(modifier: Modifier = Modifier) {
+    CircularProgressIndicator(modifier = modifier)
+}
